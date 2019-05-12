7fc29eba854028ee6154eaafab0a8f57b2028fc1
pjbb <paulaj.benitezb@gmail.com>
2019-05-12 11:17:09 -0500

    WEB-INF
    Create new clases .js for cargo, dia, evaluacion and horarioAlimentacion

---
+++ src/main/webapp/js/vendor/cargo.js
@@ -0,0 +1,49 @@
+(function ($) {
+    $(document).ready(function (event) {
+        $.ajax({
+            method: 'GET',
+            url: '/Zoo/api/cargos',
+            dataType: 'json',
+            contentType: 'application/json'
+        }).done(function (data) {
+            for (var i = 0; i < data.length; i++) {
+                var tableRow = $('<tr>');
+                // División de la fila
+                var nombreCargo= $('<td>');
+                nombreCargo.text(data[i].nombreCargo);
+
+                var acciones = $('<td>');
+                var botonEliminar = $('<button>').addClass('btn btn-danger');
+                botonEliminar.text('Eliminar').attr('data-id',data[i].id);
+                botonEliminar.click(eliminar);
+                var botonActualizar = $('<a href="/zoo/crearCargo.html?id='+ data[i].id+'">').addClass('btn btn-primary');
+                botonActualizar.text('Actualizar');
+
+                acciones.append(botonEliminar);
+                acciones.append(botonActualizar);
+
+                tableRow.append(nombreCargo);
+                tableRow.append(acciones);
+
+                $('#tableCargos table tbody').append(tableRow);
+            }
+            console.log(data);
+        }).fail(function (xhr, status, error) {
+            console.log(error);
+        });
+    });
+    
+    function eliminar(event){
+        $.ajax({
+            method: 'DELETE',
+            url:'/Zoo/api/cargos/'+$(this).attr('data-id'),
+            contentType: 'application/json',
+            dataType: 'json'
+        }).done(function(data){
+            console.log("Elemento eliminado");
+            window.location.reload();
+        }).fail(function(xhr, status, error){
+            console.log(error);
+        });
+    }
+})(jQuery);
---
+++ target/Zoo-1.0-SNAPSHOT/js/vendor/evaluacion.js
@@ -0,0 +1,52 @@
+(function ($) {
+    $(document).ready(function (event) {
+        $.ajax({
+            method: 'GET',
+            url: '/Zoo/api/evaluaciones',
+            dataType: 'json',
+            contentType: 'application/json'
+        }).done(function (data) {
+            for (var i = 0; i < data.length; i++) {
+                var tableRow = $('<tr>');
+                // División de la fila
+                var valor= $('<td>');
+                valor.text(data[i].valor);
+                var observacion= $('<tr>');
+                observacion.text(data[i].observacion);
+
+                var acciones = $('<td>');
+                var botonEliminar = $('<button>').addClass('btn btn-danger');
+                botonEliminar.text('Eliminar').attr('data-id',data[i].id);
+                botonEliminar.click(eliminar);
+                var botonActualizar = $('<a href="/zoo/crearEvaluacion.html?id='+ data[i].id+'">').addClass('btn btn-primary');
+                botonActualizar.text('Actualizar');
+
+                acciones.append(botonEliminar);
+                acciones.append(botonActualizar);
+
+                tableRow.append(valor);
+                tableRow.append(observacion);
+                tableRow.append(acciones);
+
+                $('#tableEvaluaciones table tbody').append(tableRow);
+            }
+            console.log(data);
+        }).fail(function (xhr, status, error) {
+            console.log(error);
+        });
+    });
+    
+    function eliminar(event){
+        $.ajax({
+            method: 'DELETE',
+            url:'/Zoo/api/evaluaciones/'+$(this).attr('data-id'),
+            contentType: 'application/json',
+            dataType: 'json'
+        }).done(function(data){
+            console.log("Elemento eliminado");
+            window.location.reload();
+        }).fail(function(xhr, status, error){
+            console.log(error);
+        });
+    }
+})(jQuery);
