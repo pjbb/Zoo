9ec236453f80f7482ac003ac643e238c0aa243c2
Charlsanm <mejia_98@hotmail.com>
2019-05-28 21:02:18 -0500

    CreateJs&HTML
    Se creo el html para crear un aminal y un cargo, y el js de animal

--- target/Zoo-1.0-SNAPSHOT/js/animal.js
+++ target/Zoo-1.0-SNAPSHOT/js/animal.js
@@ -1,7 +1,49 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+(function ($) {
+    $(document).ready(function (event) {
+        $.ajax({
+            method: 'GET',
+            url: '/Zoo/api/animal',
+            dataType: 'json',
+            contentType: 'application/json'
+        }).done(function (data) {
+            for (var i = 0; i < data.length; i++) {
+                var tableRow = $('<tr>');
+                // División de la fila
+                var nombreCargo= $('<td>');
+                nombreCargo.text(data[i].nombreCargo);
 
+                var acciones = $('<td>');
+                var botonEliminar = $('<button>').addClass('btn btn-danger');
+                botonEliminar.text('Eliminar').attr('data-id',data[i].id);
+                botonEliminar.click(eliminar);
+                var botonActualizar = $('<a href="/zoo/crearAnimal.html?id='+ data[i].id+'">').addClass('btn btn-primary');
+                botonActualizar.text('Actualizar');
 
+                acciones.append(botonEliminar);
+                acciones.append(botonActualizar);
+
+                tableRow.append(nombreCargo);
+                tableRow.append(acciones);
+
+                $('#tableAnimal table tbody').append(tableRow);
+            }
+            console.log(data);
+        }).fail(function (xhr, status, error) {
+            console.log(error);
+        });
+    });
+    
+    function eliminar(event){
+        $.ajax({
+            method: 'DELETE',
+            url:'/Zoo/api/animal/'+$(this).attr('data-id'),
+            contentType: 'application/json',
+            dataType: 'json'
+        }).done(function(data){
+            console.log("Elemento eliminado");
+            window.location.reload();
+        }).fail(function(xhr, status, error){
+            console.log(error);
+        });
+    }
+})(jQuery);
